8ed644bc30e1ffc27b1f7445c87134ca9542556c
ajp15 <aishwarya.pattar15@imperial.ac.uk>
2019-01-22 15:02:32 +0000

    Add files via upload
    Sending DC signal from one channel wirelessly

---
+++ Test_1Signal.ino
@@ -0,0 +1,20 @@
+// Code for testing wireless transmission of signals from 
+// one channel
+
+// input signal goes to pin A6
+int input = A6;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read voltage signal and map to mV
+  double mV = map(analogRead(input), 0, 1023, 0, 5000);
+
+  // print to serial monitor
+  Serial.println(mV);
+
+  // delat 20ms
+  delay(20);
+}
