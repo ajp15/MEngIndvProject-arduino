39b90c204a957d21bd725c145548f5bd6918ce9f
Aishwarya Pattar <aishwarya.pattar15@imperial.ac.uk>
2019-05-30 15:29:21 +0100

    implemented Low Pass Filter at 1Hz

--- SPI_transfer.ino
+++ SPI_transfer.ino
@@ -2,6 +2,8 @@
 #include <SPI.h>
 #include "Adafruit_BluefruitLE_SPI.h"
 #include "BluefruitConfig.h"
+#include <Filters.h>
+
 
 #if SOFTWARE_SERIAL_AVAILABLE
   #include <SoftwareSerial.h>
@@ -23,6 +25,11 @@ void error(const __FlashStringHelper*err) {
 int Kpin = A7;
 int Gpin = A0;
 int Lpin = A1;
+
+float filterfreq = 1.0;
+FilterOnePole lowpassFilterK(LOWPASS, filterfreq);
+FilterOnePole lowpassFilterG(LOWPASS, filterfreq);
+FilterOnePole lowpassFilterL(LOWPASS, filterfreq);
 
 /**************************************************************************/
 
@@ -71,22 +78,28 @@ void loop() {
 
   // read signal from potassium pin and send
   unsigned long timeK = millis();
-  int Kval = map(analogRead(Kpin), 0, 1023, 0, 5000);
-  String Koutput = "K" + String(timeK) + " " + String(Kval);
+  int K = lowpassFilterK.input(analogRead(Kpin));
+  int Kval = map(K, 0, 1023, 0, 5000);
+  String Koutput = "K" + String(timeK) + " " + String(Kval) + "\n";
+  Serial.print(Koutput);
   ble.print(Koutput);
   delay(200);
 
   // read signal from glucose pin and send
   unsigned long timeG = millis();
-  int Gval = map(analogRead(Gpin), 0, 1023, 0, 5000);
-  String Goutput = "G" + String(timeG) + " " + String(Gval);
+  int G = lowpassFilterG.input(analogRead(Gpin));
+  int Gval = map(G, 0, 1023, 0, 5000);
+  String Goutput = "G" + String(timeG) + " " + String(Gval) + "\n";
+  Serial.print(Goutput);
   ble.print(Goutput);
   delay(200);
 
   // read signal from lactate pin and send
   unsigned long timeL = millis();
-  int Lval = map(analogRead(Lpin), 0, 1023, 0, 5000);
-  String Loutput = "L" + String(timeG) + " " + String(Lval);
+  int L = lowpassFilterL.input(analogRead(Lpin));
+  int Lval = map(L, 0, 1023, 0, 5000);
+  String Loutput = "L" + String(timeG) + " " + String(Lval) + "\n";
+  Serial.print(Loutput);
   ble.print(Loutput); 
 
   delay(600);
